46f9b8a25d250fb87daecad1bba83ec96eff9af0
Roman Zubcov <76443550+RomanZubcov@users.noreply.github.com>
2023-05-30 12:39:01 +0300

    Add files via upload

---
+++ missingvalue.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	int i;
+	for (i = 0; i <= 5; i++)
+
+	{
+		if (i % 2 == 1)
+		{ 
+			int i = 9;
+		++i;
+		printf("%d", i);
+	}
+	else
+	{
+		int i = 9;
+		--i;
+		printf("%d", i);
+	}
+
+	return 0;
+}
